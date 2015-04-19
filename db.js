4e8793ff670be908aec442c40272cfe268be1313
yeshavanta <yeshavanta.kp@gmail.com>
2015-04-20 00:28:03 +0530

    db

--- db.js
+++ db.js
@@ -2,9 +2,9 @@
  * Created by yash on 4/18/2015.
  */
 
-var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/ridesharedatabase',function(){
-    console.log('Connected to the database');
-});
 
+var mongoose = require('mongoose');
+
+var url = process.env.MONGOLAB_URI || 'mongodb://localhost/ridesharedatabase';
+mongoose.connect(url);
 module.exports = mongoose;
