c5dfa89e288597cb85af3e27d6f6607249ada757
ykp <ykp@ykps-MacBook-Pro.local>
2015-08-18 00:48:20 -0700

    dbrestart

--- db.js
+++ db.js
@@ -5,9 +5,13 @@
 
 var mongoose = require('mongoose');
 
-//var url = process.env.MONGOLAB_URI || 'mongodb://localhost/ridesharedatabase';
-var url = 'mongodb://yeshridesharedatabase:rasenganridesharedatabase@ec2-52-74-203-31.ap-southeast-1.compute.amazonaws.com:27017/ridesharedatabase';
+//var url = process.env.MONGOLAB_URI || 'mongodb://localhost/ridesharedatabase'; ec2-52-76-63-147.ap-southeast-1.compute.amazonaws.com
+var url = 'mongodb://yeshridesharedatabase:rasenganridesharedatabase@ec2-52-76-63-147.ap-southeast-1.compute.amazonaws.com:27017/ridesharedatabase';
 mongoose.connect(url,function(err){
-    console.log('Error while connecting to MongoDB: '+err);
+    if(err){
+        console.log('Error while connecting to MongoDB: '+err);
+    }else{
+        console.log('Yaay connected to MongoDB: ');
+    }
 });
 module.exports = mongoose;
